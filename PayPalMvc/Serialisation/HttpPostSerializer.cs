b66b9a3ece00bb3ca4298ef124485fdd518db646
jhelmink <jhelmink@localhost>
2013-05-23 12:29:20 +0100

    Modified Serializer to handle List objects

--- PayPalMvc/Serialisation/HttpPostSerializer.cs
+++ PayPalMvc/Serialisation/HttpPostSerializer.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Web;
+using System.Collections;
 
 namespace PayPalMvc {
 	/// <summary>
@@ -15,9 +16,11 @@ namespace PayPalMvc {
 		/// Serializes an object to a format usable for an HTTP POST. 
 		/// All public instance properties are serialized. 
 		/// </summary>
-		public string Serialize(object toSerialize) {
+		public string Serialize(object toSerialize) 
+        {
 			var type = toSerialize.GetType();
 			var pairs = new Dictionary<string, string>();
+            var subItems = string.Empty;
 
 			foreach (var property in GetProperties(type)) {
 				if (!property.CanRead) continue;
@@ -33,20 +36,35 @@ namespace PayPalMvc {
 					convertedValue = HttpUtility.UrlEncode(convertedValue, Encoding.GetEncoding("ISO-8859-15"));
 				}
 
+                // Serialize any Lists of objects
+                if (property.PropertyType.FullName.Contains("List"))
+                {
+                    IList propValue = (IList)property.GetValue(toSerialize, null);
+                    for (int i = 0; i < propValue.Count; i++)
+                    {
+                        // This is a bit of hack for the serialization of the ExpressCheckoutItems to have an item index
+                        string serializedItem = Serialize(propValue[i]).Replace("_mIndex", i.ToString());
+                        subItems = subItems + "&" + serializedItem;
+                    }
+                    continue;
+                }
+
 				pairs.Add(property.Name, convertedValue);
 			}
 
 			var result = from pair in pairs
 			             select pair.Key + "=" + pair.Value;
 
-			return string.Join("&", result.ToArray());
+            return string.Join("&", result.ToArray()) + subItems;
 		}
 
-		static IEnumerable<PropertyInfo> GetProperties(Type type) {
+		static IEnumerable<PropertyInfo> GetProperties(Type type) 
+        {
 			return type.GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.InvokeMethod);
 		}
 
-		static string GetFormat(PropertyInfo property) {
+		static string GetFormat(PropertyInfo property)
+        {
 			var attribute = (FormatAttribute) Attribute.GetCustomAttribute(property, typeof (FormatAttribute));
 
 			if (attribute != null) {
@@ -56,11 +74,13 @@ namespace PayPalMvc {
 			return "{0}";
 		}
 
-		static bool ShouldEncode(PropertyInfo property) {
+		static bool ShouldEncode(PropertyInfo property) 
+        {
 			return !Attribute.IsDefined(property, typeof (UnencodedAttribute));
 		}
 
-		static bool IsOptional(PropertyInfo property) {
+		static bool IsOptional(PropertyInfo property) 
+        {
 			return Attribute.IsDefined(property, typeof (OptionalAttribute));
 		}
 	}
